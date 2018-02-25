570ea720da260ebd055b4680c9451bdbf7c4a833
valePV <va.perez19@hotmail.com>
2018-02-25 02:31:33 -0300

    base de datos

---
+++ assets/js/data.js
@@ -0,0 +1,78 @@
+// base de datos
+var products = [
+  { ID: 'coo1', img: 'nofound.jpg', name: 'iPhone X', type: 'Celulares', 
+  precio: '$754.990', info: '64GB - Space Gray'
+  },
+  { ID: 'coo2', img: 'nofound.jpg', name: 'iPhone 8', type: 'Celulares', 
+  precio: '$538.990', info: '64GB - Silver'
+  },
+  { ID: 'coo3', img: 'nofound.jpg', name: 'Samsung Galaxy S(', type: 'Celulares', 
+  precio: '$449.900', info: 'Plus Dual Sim 64GB'
+  },
+  { ID: 'coo4', img: 'nofound.jpg', name: 'Samsung Galaxy Note 8', type: 'Celulares', 
+  precio: '$549.900', info: 'DualSim 64GB'
+  },
+  { ID: 'coo5', img: 'nofound.jpg', name: 'Samsung Galaxy J5', type: 'Celulares', 
+  precio: '$179.900', info: 'Pro 2017 - 16GB - Rosado'
+  },
+  { ID: 'coo6', img: 'nofound.jpg', name: 'Xiaomi Mi A1', type: 'Celulares', 
+  precio: '$199.990', info: '64GB - Negro'
+  },
+  { ID: 'coo7', img: 'nofound.jpg', name: 'Sony Xperia XA1', type: 'Celulares', 
+  precio: '$224.990', info: '32GB - Negro'
+  },
+  { ID: 'coo8', img: 'nofound.jpg', name: 'Huawei 10 Pro', type: 'Celulares', 
+  precio: '$518.900', info: '128GB - Mate'
+  },
+  { ID: 'too1', img: 'nofound.jpg', name: 'Lenovo Phablet 7', type: 'Tablet', 
+  precio: '$89.990', info: '16GB - Android'
+  },
+  { ID: 'too2', img: 'nofound.jpg', name: 'Mlab Mb5', type: 'Tablet', 
+  precio: '$59.990', info: 'Quadcore 9 - Full HD'
+  },
+  { ID: 'too3', img: 'nofound.jpg', name: 'Samsung Galaxy A6 T-280', type: 'Tablet', 
+  precio: '$109.990', info: '8GB - 7'
+  },
+  { ID: 'too4', img: 'nofound.jpg', name: 'Xiaomi MiPad2', type: 'Tablet', 
+  precio: '$235.690', info: '16GB - 7.9'
+  },
+  { ID: 'too5', img: 'nofound.jpg', name: 'Huawei MediaPad', type: 'Tablet', 
+  precio: '$252.857', info: 'M2 Lite'
+  },
+  { ID: 'too6', img: 'nofound.jpg', name: 'Alcatel Pixi', type: 'Tablet', 
+  precio: '$96.721', info: '8GB - Negra - 7'
+  },
+  { ID: 'too7', img: 'nofound.jpg', name: 'Lenovo P8', type: 'Tablet', 
+  precio: '$119.900', info: '3GB - 8'
+  },
+  { ID: 'too8', img: 'nofound.jpg', name: 'Onda V10 PRO', type: 'Tablet', 
+  precio: '$136.600', info: '4GB - Android'
+  },
+  { ID: 'soo1', img: 'nofound.jpg', name: 'N-Space IP54', type: 'Smartwatches', 
+  precio: '$44.990', info: '64GB - Iphone/Android'
+  },
+  { ID: 'soo2', img: 'nofound.jpg', name: 'Samsung Gear', type: 'Smartwatches', 
+  precio: '$238.900', info: 'S3 - Frontier'
+  },
+  { ID: 'soo3', img: 'nofound.jpg', name: 'Samsung Gear', type: 'Smartwatches', 
+  precio: '$239.900', info: 'S3 Classic'
+  },
+  { ID: 'soo4', img: 'nofound.jpg', name: 'Q 18', type: 'Smartwatches', 
+  precio: '$59.990', info: '3G - WIFI'
+  },
+  { ID: 'soo5', img: 'nofound.jpg', name: 'Xiaomi', type: 'Smartwatches', 
+  precio: '$29.990', info: 'SmartBand'
+  },
+  { ID: 'soo6', img: 'nofound.jpg', name: 'Lhotse SM35', type: 'Smartwatches', 
+  precio: '$39.990', info: 'SmartBand'
+  },
+  { ID: 'soo7', img: 'nofound.jpg', name: 'GTO8', type: 'Smartwatches', 
+  precio: '$9.790', info: 'Iphone/Android'
+  },
+  { ID: 'soo8', img: 'nofound.jpg', name: 'Diggro', type: 'Smartwatches', 
+  precio: '$37.338', info: 'Bluetooth - Siri Smart'
+  },
+
+  
+ 
+];
