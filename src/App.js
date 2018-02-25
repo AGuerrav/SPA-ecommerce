fb64335b3721b72e9d8273cebeebd69b585c999a
meliveloz <mvelozcampos@gmail.com>
2018-02-25 13:30:33 -0300

    react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import MenuSection from './sections/menu.js'
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <MenuSection />
+      </div>
+    );
+  }
+}
+
+export default App;
