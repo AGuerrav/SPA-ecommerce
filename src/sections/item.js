0caf3ac714fac90ec466053aff11a80ca31d6076
meliveloz <mvelozcampos@gmail.com>
2018-02-26 08:28:31 -0300

    libre comercio

---
+++ src/sections/item.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class Item extends Component {
+ static propTypes ={
+  title: PropTypes.string,
+  thumbnail: PropTypes.string,
+  condition: PropTypes.string,
+  price: PropTypes.string
+ }
+
+ render(){
+  const {thumbnail, title, condition, price} = this.props
+
+  return (
+    <div className="thumbnail">
+      <img className="img-responsive" src={thumbnail} alt={title} />
+      <div className="caption">
+        <h2>{title}</h2>
+        <h3>{condition}</h3>
+        <h3>${price}</h3>
+      </div>
+    </div>
+    )
+ }
+ }
